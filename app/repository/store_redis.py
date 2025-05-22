f47e58901082c3f062ce8f4a7bd07e34e091cfb6
alves-dev <alvesmoreiraigor@gmail.com>
2025-05-22 07:49:26 -0300

    feat: added key prefix

--- app/repository/store_redis.py
+++ app/repository/store_redis.py
@@ -7,6 +7,7 @@ from app.repository.data_store import DataStore
 
 class DataStoreRedis(DataStore):
     def __init__(self, host='localhost', port=6379, username=None, password=None, db=0):
+        self.prefix = 'repo-inspector:'
         self.client = redis.Redis(
             host=host,
             port=port,
@@ -17,17 +18,17 @@ class DataStoreRedis(DataStore):
         )
 
     def add(self, key: str, value: str):
-        self.client.set(key, value)
+        self.client.set(f'{self.prefix}{key}', value)
 
     def get(self, key: str):
-        return self.client.get(key)
+        return self.client.get(f'{self.prefix}{key}')
 
     def add_json(self, key: str, value: dict):
         json_value = json.dumps(value)
-        self.client.set(key, json_value)
+        self.client.set(f'{self.prefix}{key}', json_value)
 
     def get_json(self, key: str) -> dict:
-        value = self.client.get(key)
+        value = self.client.get(f'{self.prefix}{key}')
         if value:
             return json.loads(value)
         return {}
